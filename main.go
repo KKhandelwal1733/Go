7c66455a6ab5240a7b846572b7bec3f2b0b8b49b
Krishna <khandelwal1733@gmail.com>
2025-12-22 16:55:16 +0530

    getEventByID implemented

--- main.go
+++ main.go
@@ -1,48 +1,62 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"time"
+	"strconv"
 
 	"example.com/myapp/db"
 	"example.com/myapp/models"
 	"github.com/gin-gonic/gin"
 )
 
-
-
 func main() {
 	db.InitDB()
-	server:=gin.Default()
-	server.GET("/events",getEvents)
-	server.POST("/events",createEvent)
-
+	server := gin.Default()
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+	server.GET("/event/:id",getEventByID)
 
 	server.Run(":8080")
 }
 
 func getEvents(c *gin.Context) {
-	events:=models.GetAllEvents()
-	c.JSON(http.StatusOK,events)}
-
-
-func createEvent(c *gin.Context){
-   var event models.Event
-   err:= c.ShouldBindJSON(&event)
-   fmt.Println(event)
-   fmt.Println(err)
-   if err!=nil{
-    c.JSON(http.StatusBadRequest,gin.H{"message":"Could not parse the data you sent!!!"})
-    return }
-   event.ID=1;
-   event.UserID=1;
-   event.Date=time.Now()
-   event.Save()
-   c.JSON(http.StatusCreated,gin.H{"messge":"Event created!","event":event})
-
-   
+	events, err := models.GetAllEvents()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Could not retrieve events"})
+		return
+	}
+	c.JSON(http.StatusOK, events)
 }
 
+func createEvent(c *gin.Context) {
+	var event models.Event
+	err := c.ShouldBindJSON(&event)
 
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the data you sent!!!"})
+		return
+	}
 
+	err = event.Save()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save event", "error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Event created successfully", "event_id": event.ID})
+
+}
+
+func getEventByID(c *gin.Context) {
+	id,err:=strconv.ParseInt(c.Param("id"),10,64)
+	if err!=nil{
+		c.JSON(http.StatusBadRequest, gin.H{"message":"Invalid event ID"})
+		return
+	}
+	event,err:=models.GetEventByID(id)
+	if err!=nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"message":"Could not retrieve event"})
+		return
+	}
+	c.JSON(http.StatusOK,*event)
+
+}
--- models/event.go
+++ models/event.go
@@ -1,22 +1,62 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/myapp/db"
+)
 
 type Event struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Date        time.Time `json:"date"`
-	Location    string `json:"location"`
-	UserID      int    `json:"user_id"`
-}
-var events = []Event{}
-
-func (e Event) Save(){
-	events=append(events,e)
-
+	ID          int64     `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	DateTime    time.Time `json:"date_time"`
+	Location    string    `json:"location"`
+	UserID      int       `json:"user_id"`
 }
 
-func GetAllEvents() []Event {
-	return events
+func (e *Event) Save() error {
+	query := "INSERT INTO events(name,description,location,date,user_id) VALUES(?,?,?,?,?)"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	res, err := stmt.Exec(e.Name, e.Description, e.Location, time.Now(), e.UserID)
+	defer stmt.Close()
+	if err != nil {
+		return err
+	}
+	id, err := res.LastInsertId()
+	e.ID = id
+	return err
+}
+
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var events []Event
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+	return events, nil
+}
+
+func GetEventByID(id int64) (*Event, error) {
+	query := "SELECT * FROM events WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+	if err != nil {
+		return nil, err
+	}		
+	return &event, nil
 }
